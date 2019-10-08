6840d69878f182e32813ef43d26d89ceaf2f2e66
strongbrent <brentg71@gmail.com>
2019-10-08 00:41:48 -0700

    Test: unattended ohmyzsh install

--- scripts/host/bootstrap-macos.sh
+++ scripts/host/bootstrap-macos.sh
@@ -8,14 +8,43 @@ source "${parentdir}/lib/functions.sh"
 
 # --- Helper Functions ---------------------------------------------------
 
+# Installs Host Packages
+install_packages() {
+    pkgs=(
+        zsh
+    )
+
+    cask_pkgs=(
+        iterm2
+    )
+
+    # install brew packages
+    for i in "${pkgs[@]}"
+    do
+        echo_task "Processing brew package: ${i}"
+        brew install -q "${i}"
+    done
+
+    # install cask packages
+    for i in "${cask_pkgs[@]}"
+    do
+        echo_task "Processing cask package: ${i}"
+        brew install -q "${i}"
+    done
+}
+
 # Installs Oh-My-Zsh
 install_ohmyzsh() {
-    brew cask install iterm2
-    brew install zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 }
+
 
 # --- Main Function ------------------------------------------------------
 main() {
+
+    echo_header "Installing: Required Packages"
+    install_packages
+
     echo_header "Installing: oh-my-zsh"
     install_ohmyzsh
 }
