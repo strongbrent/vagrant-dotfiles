c4e4667d2592a271eb58e7ddcdfeba11b4ee59d7
strongbrent <brentg71@gmail.com>
2019-10-08 22:51:04 -0700

    Added all the missing pieces and refactored

--- scripts/host/bootstrap-macos.sh
+++ scripts/host/bootstrap-macos.sh
@@ -17,18 +17,69 @@ shell_envs=(
 
 # --- Helper Functions ---------------------------------------------------
 
+# Creates a passwordless sudo entry
+create_sudoer() {
+    local -r tmp_path="/tmp/${USER}"
+    local -r sudoers_dir="/etc/sudoers.d"
+    local -r sudoers_path="${sudoers_dir}/${USER}"
+
+    if found_file "${sudoers_path}"; then
+        echo_task "Sudoers file already created: ${sudoers_path}"
+        return
+    fi
+
+    echo_task "Creating sudoers file: ${sudoers_path}"
+    touch "${tmp_path}"
+    chmod 0600 "${tmp_path}"
+    echo "${USER} ALL = (ALL) NOPASSWD: ALL" > "${tmp_path}"
+    sudo chown root:root "${tmp_path}"
+    sudo mv "${tmp_path}" ${sudoers_path}
+}
+
+# DESC: checks to see if a brew formula is installed
+# ARGS: $1 (REQ): name of brew formula
+# OUT:  0  -> if found
+#       !0 -> if not found
+found_brew() {
+    brew list "${1}" &>/dev/null
+}
+
+# DESC: checks to see if a brew cask is installed
+# ARGS: $1 (REQ): name of the brew cask
+# OUT:  0  -> if found
+#       !0 -> if not found
+found_cask() {
+    brew cask list "${1}" &>/dev/null
+}
+
+# DESC: installs a specified brew formula/cask
+# ARGS: $1 (REQ): either 'cask' or name of the brew formula
+#       $2 (OPT): name of the brew cask
+install_brew() {
+    if [ "$1" == 'cask' ]; then
+        if found_cask "${2}"; then
+            echo_task "Already installed brew cask: ${2}"
+            return
+        fi
+
+        echo_task "Installing brew cask: ${2}"
+        brew install "${2}"
+        return
+    fi
+
+    if found_brew "${1}"; then
+        echo_task "Already installed brew formula: ${1}"
+        return
+    fi
+
+    echo_task "Installing brew formula: ${1}"
+    brew install "${1}"
+}
+
 # Installs Oh-My-Zsh
 install_ohmyzsh() {
-    local -r shell_pkg="zsh"
     local -r pkg="oh-my-zsh"
     local -r pkg_dir="${HOME}/.${pkg}"
-
-    if found_cmd "${shell_pkg}"; then
-        echo_task "Shell package already installed: ${shell_pkg}"
-    else
-        echo_task "Installing shell package: ${shell_pkg}"
-        brew install "${shell_pkg}"
-    fi
 
     if found_dir "${pkg_dir}"; then
         echo_task "Package already installed: ${pkg}"
@@ -77,18 +128,81 @@ install_ohmyzsh() {
     done
 }
 
+# Installs specified packages
+install_packages() {
+    pkgs=(
+        cmake
+        coreutils
+        curl
+        dos2unix
+        fontconfig
+        freetype
+        gettext
+        git
+        gnupg
+        gnutls
+        htop
+        iftop
+        jpeg
+        jq
+        openssh
+        openssl
+        pass
+        pidof
+        pwgen
+        readline
+        telnet
+        tmux
+        trash
+        tree
+        vim
+        wget
+        xz
+        youtube-dl
+        zlib
+        zsh
+    )
+
+    casks=(
+        dbeaver-community
+        firefox
+        iterm2
+        kindle
+        postman
+        robo-3t
+        slack
+        spotify
+        teamviewer
+        textmate
+        the-unarchiver
+        transmission
+        tunnelblick
+        vagrant
+        vagrant-manager
+        virtualbox-extension-pack
+        visual-studio-code
+        wireshark
+    )
+
+    brew update
+
+    for i in "${pkgs[@]}"
+    do
+        echo_task "Processing brew formula: ${i}"
+        install_brew "${1}"
+    done
+
+    for i in "${casks[@]}"
+    do
+        echo_task "Processing brew casks: ${i}"
+        install_brew cask "${1}"
+    done
+}
+
 # Installs Ultimate vimrc
 install_vimrc() {
-    local -r ed_pkg="vim"
     local -r pkg_name="Ultimate .vimrc"
     local -r pkg_dir="${HOME}/.vim_runtime"
-
-    if found_cmd "${ed_pkg}"; then
-        echo_task "Editor package already installed: ${ed_pkg}"
-    else
-        echo_task "Installing editor package: ${ed_pkg}"
-        brew install "${ed_pkg}"
-    fi
 
     if found_dir "${pkg_dir}"; then
         echo_task "Package already installed: ${pkg_name}"
@@ -118,15 +232,61 @@ install_vimrc() {
     done
 }
 
+# Installs VS Code extensions
+install_vs_code_extensions() {
+    exts=(
+        donjayamanne.githistory
+        ms-vscode-remote.vscode-remote-extensionpack
+        yzhang.markdown-all-in-one
+    )
+
+    for i in "${exts[@]}"
+    do
+        if code --list-extensions | grep "${i}" &> /dev/null; then
+            echo_task "VS Code Extension already installed: ${i}"
+            continue
+        fi
+
+        echo_task "Installing VS Code Extension: ${i}"
+        code --install-extension "${i}"
+    done
+}
+
+# Changes the default shell to ZSH
+modify_shell() {
+    local -r current_shell=$(echo ${SHELL})
+    local -r new_shell=$(command -v zsh)
+
+    if [ ${current_shell} == ${new_shell} ]; then
+        echo_task "Shell is already set to use: ${current_shell}"
+        return
+    fi
+
+    echo_task "Setting SHELL to use: ${new_shell}"
+    chsh -s "${new_shell}"
+}
+
 
 # --- Main Function ------------------------------------------------------
 main() {
+
+    echo_header "Creating: passwordless sudo"
+    create_sudoer
+
+    echo_header "Installing: Required Packages"
+    install_packages
 
     echo_header "Installing: oh-my-zsh"
     install_ohmyzsh
 
     echo_header "Installing: Ultimate .vimrc"
     install_vimrc
+
+    echo_header "Installing: VS Code Extensions"
+    install_vs_code_extensions
+
+    echo_header "Switching Default Shell: zsh"
+    modify_shell
 }
 
 
